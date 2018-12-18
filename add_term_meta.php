729c2cd2d44446f5e41eea219f4281332599083f
asabagh <a.sabagh72@gmail.com>
2018-12-18 18:37:30 +0330

    update function simpler

--- add_term_meta.php
+++ add_term_meta.php
@@ -3,9 +3,9 @@
 function rng_TAXMETA_INPUT_NAME_add() {
 
 	?>
-	<p>sample input</p>
-	<input type="text" name="input_name" vlaue="">
-	<?php
+<p>sample input</p>
+<input type="text" name="input_name" vlaue="">
+<?php
 }
 
 function rng_TAXMETA_INPUT_NAME_created( $term_id ) {
@@ -18,26 +18,22 @@ function rng_TAXMETA_INPUT_NAME_created( $term_id ) {
 function rng_TAXMETA_INPUT_NAME_edit( $term ) {
 	$input_name	 = get_term_meta( $term->term_id, 'TAXMETA_INPUT_NAME', TRUE );
 	?>
-	<table class="form-table">
-		<tbody>
-			<tr class="form-field form-required term-name-wrap">
-				<th scope="row"><label for="name">sample input</label></th>
-				<td>	
-					<input type="text"  name="TAXMETA_INPUT_NAME" value="<?php echo $input_name; ?>" />	
-				</td>
-			</tr>
-		</tbody>
-	</table>
-	<?php
+<table class="form-table">
+    <tbody>
+        <tr class="form-field form-required term-name-wrap">
+            <th scope="row"><label for="name">sample input</label></th>
+            <td>
+                <input type="text" name="TAXMETA_INPUT_NAME" value="<?php echo $input_name; ?>" />
+            </td>
+        </tr>
+    </tbody>
+</table>
+<?php
 }
 
 function rng_TAXMETA_INPUT_NAME_update( $term_id ) {
 	$input_name	 = $_POST[ 'TAXMETA_INPUT_NAME' ];
-	if ( !empty( $input_name ) ) {
-		update_term_meta( $term_id, 'TAXMETA_NAME', $input_name );
-	} else {
-		add_term_meta( $term_id, 'TAXMETA_NAME', $input_name );
-	}//endif
+	add_term_meta( $term_id, 'TAXMETA_NAME', $input_name );
 }
 
 add_action( 'TAXONOMY_add_form_fields', 'rng_TAXMETA_INPUT_NAME_add', 50 );
