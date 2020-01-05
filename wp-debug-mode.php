2781cc27acb0f7ef0a3e72536f1f32b2e97beafc
Abolfazl Sabagh <a.sabagh72@gmail.com>
2020-01-05 11:16:53 +0330

    wp debug mode in wordpress

---
+++ wp-debug-mode.php
@@ -0,0 +1,7 @@
+<?php
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+define( 'SCRIPT_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+define( 'SAVEQUERIES', true );
